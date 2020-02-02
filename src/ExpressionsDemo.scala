386332bc1c673c6c04a9ee779f31ec7bae69a2cd
krishna sandeep <vanamasandeep2019@gmail.com>
2020-02-02 16:39:14 +0530

    Class 2 : Expresions Demo

---
+++ src/ExpressionsDemo.scala
@@ -0,0 +1,31 @@
+
+
+object ExpressionsDemo extends App{
+  
+  val x=1+2+3; // Expression
+  
+  println(x);
+  
+  println(2+3*4);
+  
+  println(x==2);
+  
+  println(x>2);
+  
+  println(x<=2);
+  
+  val cond=true
+  
+  val condValue=if(cond) 5 else 10
+  
+  println(condValue);
+  
+  
+  val cond1=false;
+  
+  val condValue1=if(cond1) 5 else 10
+  
+  println(condValue1);
+  
+  
+}
