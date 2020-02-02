f38a3af753621dcb2202ec5328dfde3cc357ac22
krishna sandeep <vanamasandeep2019@gmail.com>
2020-02-02 16:50:49 +0530

    Class 3 : Blocks Demo

---
+++ src/CodeBlocks.scala
@@ -0,0 +1,15 @@
+/*Code Block is multiple lines of code 
+that are enclosed between braces i.e. everything 
+between { and } is in one block code.*/
+
+
+object CodeBlocks extends App{
+  
+  val block={
+    val x=10;
+    val y=10;
+    if(x==y) true else false
+    
+  }
+  println(block)
+}
